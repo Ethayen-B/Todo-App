0b51b9e3e9e15a4da169ab4baa7c9f8f4b6eca7e
ethayen <etbay24@gmail.com>
2025-11-17 13:27:02 +0300

    darkmode local storage

--- src/lib/ThemeStore.ts
+++ src/lib/ThemeStore.ts
@@ -1,11 +1,20 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface ThemeStoreState{
     theme: string,
     setTheme: (value: string) => void
 }
 
-export const useThemeStore = create<ThemeStoreState>((set) => ({
-    theme: 'light',
-    setTheme: (value) => set({theme: value})
-}))
+export const useThemeStore = create<ThemeStoreState>(
+    persist((set) => ({
+        theme: 'light',
+        setTheme: (value) => set({theme: value})
+    }),
+    {
+        name: 'Theme-storage'
+    }
+    )
+
+    
+)
